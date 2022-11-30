248a37ab07f171fe2551106b22c7405ca18f7f41
oscarheimdahl <o.heimdahl@gmail.com>
2022-11-30 14:38:15 +0100

    Weee

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { serve } from 'https://deno.land/std@0.166.0/http/server.ts';
 
 const handler = (_req: Request) => {
-  console.log('weeee');
-
-  return new Response('', { status: 200 });
+  return new Response('Deno is cool beans', { status: 200 });
 };
 
 await serve(handler, { port: 3002 });
